114d2b126152757de2d17ff21e851728704ef736
Scott Randolph <srandolph@tetrasys.org>
2020-09-02 01:33:11 +0000

    added subtheme

--- web/themes/custom/bfd_subtheme/gulpfile.js
+++ web/themes/custom/bfd_subtheme/gulpfile.js
@@ -2,29 +2,39 @@
  * @file
  * Provides Gulp configurations and tasks for Bootstrap for Drupal theme.
  */
+
 'use strict';
+
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], () => {
+
+
+// CSS task
+function sass2css() {
+  return gulp
+    .src('./assets/scss/style.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(browserSync.stream())
+}
+
+// Watch files
+function watchFiles() {
   browserSync.init({
-    proxy: 'http://YOUR--DEV-URL.COM'
+    open: false,
+    server: "./assets/scss/**/*.scss"
   });
 
   gulp
-    .watch('assets/scss/**/*.scss', ['sass'])
+    .watch("./assets/scss/**/*.scss", sass2css)
     .on('change', browserSync.reload);
-});
-// Compile sass into CSS & auto-inject into browsers.
-gulp.task('sass', () =>
-  gulp
-  .src('assets/scss/style.scss')
-  .pipe(sass())
-  .pipe(autoprefixer())
-  .pipe(gulp.dest('assets/css'))
-  .pipe(browserSync.stream())
-);
+}
 
-gulp.task('default', ['serve']);
+const watch = gulp.series(watchFiles);
+
+// export tasks
+
+exports.default = watch;
